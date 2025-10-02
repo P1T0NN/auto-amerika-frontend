face9495506e3cd469855ceeabdf704cecfd40f2
P1T0NN <ognjen.tapuskovic@gmail.com>
2025-10-02 21:04:07 +0200

    Returning response in guardedQuery instead of redirecting

--- src/features/auth/utils/auth-utils.ts
+++ src/features/auth/utils/auth-utils.ts
@@ -1,9 +1,8 @@
 // SVELTEKIT IMPORTS
 import { getRequestEvent, query } from '$app/server';
-import { redirect } from '@sveltejs/kit';
 
-// CONFIG
-import { UNPROTECTED_PAGE_ENDPOINTS } from '@/shared/config';
+// LIBRARIES
+import { m } from "@/shared/lib/paraglide/messages";
 
 export const guardedQuery = <T, TArgs extends any[]>(fn: (sessionToken: string, ...args: TArgs) => T) => {
     return query((...args: TArgs) => {
@@ -11,7 +10,11 @@ export const guardedQuery = <T, TArgs extends any[]>(fn: (sessionToken: string, 
         const sessionToken = event.cookies.get("session_token");
 
         if (!sessionToken) {
-            redirect(307, UNPROTECTED_PAGE_ENDPOINTS.LOGIN_PAGE);
+            return {
+                success: false,
+                message: m["GenericMessages.USER_NOT_AUTHENTICATED"](),
+                data: null
+            };
         }
 
         return fn(sessionToken, ...args);
